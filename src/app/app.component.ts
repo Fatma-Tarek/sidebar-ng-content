55b430ae19a4a95c0bacb973762666181315756d
Fatma Tarek <fatma.tarek204@gmail.com>
2021-09-06 16:57:25 +0200

    solving animation of sidebar finally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,71 @@
 import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { trigger , state , style, transition, animate, keyframes} from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  /*animations: [
+    trigger('list1', [
+      //in is just dumy data 
+      state('in', style({
+        opacity: 1, //fully visiable
+         transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity:0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(500)
+      ]),
+      transition('* => void',[
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      //in is just dumy data 
+      state('in', style({
+        opacity: 1, //fully visiable
+         transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)', 
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)', 
+            opacity: 0.5,
+            offset: .3
+          }),
+          style({
+            transform: 'translateX(-20px)', 
+            opacity: 1,
+            offset: .8
+          }),
+          style({
+            transform: 'translateX(0px)', 
+            opacity: 1,
+            offset: 1
+          }),
+        ]))
+      ]),
+      transition('* => void',[
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+  ]*/
 })
 export class AppComponent implements OnDestroy {
   @ViewChild('f') signupForm: NgForm;
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,23 +1,83 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GlobalServices } from '../global.service';
+import { trigger , state , style, transition, animate, keyframes} from '@angular/animations'
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
+  animations: [
+    trigger('list1', [
+      //in is just dumy data 
+      state('in', style({
+        opacity: 1, //fully visiable
+         transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity:0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(500)
+      ]),
+      transition('* => void',[
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list2', [
+      //in is just dumy data 
+      state('in', style({
+        opacity: 1, //fully visiable
+         transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)', 
+            opacity: 0,
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)', 
+            opacity: 0.5,
+            offset: .3
+          }),
+          style({
+            transform: 'translateX(-20px)', 
+            opacity: 1,
+            offset: .8
+          }),
+          style({
+            transform: 'translateX(0px)', 
+            opacity: 1,
+            offset: 1
+          }),
+        ]))
+      ]),
+      transition('* => void',[
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+  ]
 })
 export class SidebarComponent implements OnInit {
-  
+  @Input() message: string='';
+  @Output() close = new EventEmitter<void>();
+  @Input() lang: string= "EN";
+  name!: string;
+
   constructor(private globalServices: GlobalServices) {
     console.log("hello from ngIf")
     console.log("lang ="+this.lang)
-    
    }
 
-  @Input() message: string='';
-  @Output() close = new EventEmitter<void>();
-  @Input() lang: string;
-  name!: string;
+  
   ngOnInit(): void {
   }
 
@@ -31,11 +91,9 @@ export class SidebarComponent implements OnInit {
 
   }
 
-
   resetForm(){
   this.globalServices.formReset.emit();
   }
-
 
   sendForm(){
     console.log("send form")
