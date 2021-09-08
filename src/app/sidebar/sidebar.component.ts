cd7e402d4f0c78c9fcc80554cbd9feedac8a924f
Fatma Tarek <fatma.tarek204@gmail.com>
2021-09-08 12:05:40 +0200

    add filter sidebar in reverse way in case of arabic language

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { GlobalServices } from '../global.service';
 import { trigger , state , style, transition, animate, keyframes} from '@angular/animations';
 import { Subject } from 'rxjs';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-sidebar',
@@ -20,6 +21,32 @@ import { Subject } from 'rxjs';
         // opacity: 0, //fully visiable
         //: 'translateX(0)'
       })),
+       /******* From any thing before Appear in case of Arabic ***********/
+       transition('void => beforeAppearArabic',[
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(100px)', 
+            opacity: 0,
+            /*offset: 1*/
+          }),
+          style({
+            transform: 'translateX(50px)', 
+            opacity: .5,
+            /*offset: .8*/
+          }),
+          style({
+            transform: 'translateX(20px)', 
+            opacity: 0.8,
+           /*offset: .3*/
+          }),
+          style({
+            transform: 'translateX(0px)', 
+            opacity: 1,
+            /*offset: 0*/
+          }),          
+        ]))
+      ]),
+      /**** End of From any thing before Appear in case of Arabic  ********/
       /******* Transition to remove from DOM **********/
       transition('void => *', [
         animate(1000, keyframes([
@@ -76,29 +103,69 @@ import { Subject } from 'rxjs';
             /*offset: 0*/
           }),          
         ]))
-      ])
+      ]),
       /**** End of From any thing before vading ********/
+      /******* From any thing before vading in case of Arabic ***********/
+      transition('* => berforeVadingArabic',[
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(0px)', 
+            opacity: 1,
+            /*offset: 1*/
+          }),
+          style({
+            transform: 'translateX(20px)', 
+            opacity: 1,
+            /*offset: .8*/
+          }),
+          style({
+            transform: 'translateX(50px)', 
+            opacity: 0.5,
+           /*offset: .3*/
+          }),
+          style({
+            transform: 'translateX(100px)', 
+            opacity: 0,
+            /*offset: 0*/
+          }),          
+        ]))
+      ]),
+      /**** End of From any thing before vading in case of Arabic  ********/
     ]),
   ]
 })
 export class SidebarComponent implements OnInit {
   @Input() message: string='';
   @Output() close = new EventEmitter<void>();
-  @Input() lang: string= "EN";
+  @Input() lang: string;
   name!: string;
   state : any;
 
   constructor(private globalServices: GlobalServices) {
     console.log("hello from ngIf")
     console.log("lang ="+this.lang)
+    console.log("before if state = "+this.state)
+    /*if(this.lang === "AR"){
+      this.state = "beforeAppearArabic"
+      console.log("inside if state = "+this.state)
+    }
+    console.log("after if state = "+this.state)*/
    }
 
   
   ngOnInit(): void {
+    if(this.lang === "AR"){
+      this.state = "beforeAppearArabic"
+      console.log("inside if state = "+this.state)
+    }
+    console.log("after if state = "+this.state)
   }
 
   onClose(){
     this.state = 'beforeVading'
+    if(this.lang === "AR"){
+      this.state = 'berforeVadingArabic'
+    }
     this.close.emit(
     );
   }
